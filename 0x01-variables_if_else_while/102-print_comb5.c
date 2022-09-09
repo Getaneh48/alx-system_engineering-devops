7c9672ea565e6c44fbc27cfd5bde4ca793132359
Getaneh48 <getaneh11@gmail.com>
2022-09-09 17:42:42 +0000

    12. Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+	int l;
+
+	for (i = 48; i <= 57; i++)
+	{
+		for (j = 48; j <= 57; j++)
+		{
+			for (k = i; k <= 57; k++)
+			{
+				for (l = j + 1; l <= 57; l++)
+				{
+					putchar(i);
+					putchar(j);
+					putchar(' ');
+					putchar(k);
+					putchar(l);
+					if (!(i == 57 && j == 56 && k == 57 && l == 57))
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
