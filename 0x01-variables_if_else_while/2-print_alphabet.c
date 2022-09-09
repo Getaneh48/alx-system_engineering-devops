dbd1b199f09bec05d6dcc47b7da31bfbe1e9fc51
Getaneh48 <getaneh11@gmail.com>
2022-09-09 13:41:34 +0000

    2. I sometimes suffer from insomnia.

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+#include <string.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+
+	putchar('\n');
+	return (0);
+}
