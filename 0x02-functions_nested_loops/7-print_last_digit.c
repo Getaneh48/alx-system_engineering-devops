2a4c04c569d7679407febfab69c5c14d99f58e11
Getaneh48 <getaneh11@gmail.com>
2022-09-13 15:27:25 +0300

    Bug Fixed

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,9 +9,14 @@
 int print_last_digit(int n)
 {
 	int last_digit;
-
-	last_digit = (n % 10) + 48;
-	_putchar(last_digit);
+	
+	if (n < 10)
+		last_digit = n + 48;
+	else
+	{
+		last_digit = n % 10;
+		_putchar(last_digit + 48);
+	}
 
 	return (last_digit);
 }
