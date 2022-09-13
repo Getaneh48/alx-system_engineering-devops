32655136ce7c788ca59703cd7b03053474d08a22
Getaneh48 <getaneh11@gmail.com>
2022-09-13 11:47:59 +0300

    Task 2. 10 x alphabet

---
+++ 0x02-functions_nested_loops/2-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+* main - Check your code
+*
+* Description:  a function that prints 10 times the alphabet,
+* in lowercase, followed by a new line
+*
+* Return: void
+*/
+void print_alphabet_x10(void)
+{
+	int i, j;
+	int max = 10;
+
+	for (j = 1; j <= max; j++)
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			_putchar(i);
+		}
+		_putchar('\n');
+	}
+}
