db7408e8df0458338a9896540861045e36077fb8
Getaneh48 <getaneh11@gmail.com>
2022-09-14 14:43:09 +0300

    Betty Fixed

--- 0x02-functions_nested_loops/101-natural.c
+++ 0x02-functions_nested_loops/101-natural.c
@@ -26,18 +26,19 @@ void print_natural_sum(void)
 	n = 1;
 	index = 1;
 	sum = 0;
-	max = 16;
+	max = 1024;
 
 	while (n > 0)
 	{
-		x1m = (x1 * index > max) ? 0 : (x1 * index);
-		x2m = ((x2 * index) > max) ? 0 : (x2 * index);
-		sum += (x1m + x2m);
+		x1m = (x1 * index >= max) ? 0 : (x1 * index);
+		x2m = ((x2 * index) >= max) ? 0 : (x2 * index);
 		if (x1m == 0 && x2m == 0)
 		{
 			n = 0;
 		}
+		sum += (x1m + x2m);
 		index += 1;
 	}
 	printf("%d\n", sum);
+	printf("index is %d", index);
 }
