8ec0911c18a00987b6a15135207ac192ebaaf9fb
Getaneh48 <getaneh11@gmail.com>
2022-09-14 06:58:42 +0000

    Task 12

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,77 @@
+#include "main.h"
+/**
+* print_times_table - prints the n times table, starting with 0.
+*
+* @n: int val
+*/
+void print_times_table(int n)
+{
+	int i, j, times, rem, div;
+
+	if (n >= 0 && n <= 15)
+	{
+		div = 0;
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				times = i * j;
+				if (times <= 9)
+				{
+					if (j > 0)
+					{
+						_putchar(' ');
+						_putchar(' ');
+						_putchar(' ');
+					}
+					_putchar(times + 48);
+				}
+				else if (times < 100)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					div = times / 10;
+					rem = times % 10;
+					_putchar(div + 48);
+					_putchar(rem + 48);
+				}
+				else
+				{
+					getNumbersOfDigit(times);
+				}
+
+				if (j < n)
+				{
+					_putchar(',');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
+
+/**
+* getNumbersOfDigit - print individual digits of a number.
+*
+* @num: int val
+*/
+void getNumbersOfDigit(int num)
+{
+	int div, rem, buf[5], aindex, m;
+
+	aindex = 0;
+
+	do {
+		div = num / 10;
+		rem = num % 10;
+		num = div;
+		buf[aindex] = rem;
+		aindex += 1;
+	} while (div >= 10);
+	buf[aindex] = div;
+	_putchar(' ');
+	for (m = aindex; m >= 0; m--)
+	{
+		_putchar(buf[m] + 48);
+	}
+}
