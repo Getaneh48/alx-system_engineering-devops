63efc76083066bca34063d09b2d78dfbf1d23ec6
Getaneh48 <getaneh11@gmail.com>
2022-09-15 07:28:24 +0000

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,17 @@
+/**
+* mul - checks for a digit (0 through 9).
+*
+* @a: first integer argument
+* @b: second integer argument
+*
+* Return: result of a multiplication of two numbers
+*/
+int mul(int a, int b)
+{
+
+	int result;
+
+	result = a * b;
+
+	return (result);
+}
