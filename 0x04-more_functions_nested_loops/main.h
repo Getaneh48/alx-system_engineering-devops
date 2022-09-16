1100cf5413244d50c764cb98cf5de480046434f1
Getaneh48 <getaneh11@gmail.com>
2022-09-16 08:29:00 +0000

    Task 12

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -16,5 +16,10 @@ void fizzbuzz(void);
 void print_diagonal(int n);
 void print_triangle(int size);
 void print_char(int c);
+void print_number(int n);
+int get_digit_length(int digit);
+int reverse_number(int digit, int length);
+void display_number(int rev_digit, int sign, int org_dig_len, int rev_dig_len);
+int power(int base, int exp);
 
 #endif /* MAIN_H */
