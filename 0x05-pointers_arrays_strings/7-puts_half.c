f1742152a4d7d7b43f40ed25fff6ae65b8e21b6a
Getaneh48 <getaneh11@gmail.com>
2022-09-19 08:01:15 -0700

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,57 @@
+#include "main.h"
+
+/**
+* puts2 -  prints half of a string, followed by a new line.
+* with the first character, followed by a new line.
+*
+* @str: first argument, character pointer
+*
+*/
+
+void puts_half(char *str)
+{
+	int len, div;
+
+	len = _strlen(str);
+
+	if (len % 2 == 0)
+	{
+		div = len / 2;
+		str = str + div;
+	}
+	else
+	{
+		div = (len - 1) / 2;
+		str = str + div;
+	}
+
+	while (*str)
+	{
+		_putchar(*str);
+		str += 1;
+	}
+
+	_putchar('\n');
+}
+
+/**
+* _strlen -  returns the length of a string.
+*
+* @s: character pointer
+*
+* Return: integer length of a string.
+*
+*/
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+	while (*s)
+	{
+		i++;
+		s += 1;
+	}
+
+	return (i);
+}
