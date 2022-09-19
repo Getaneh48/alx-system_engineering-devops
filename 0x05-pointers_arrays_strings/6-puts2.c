1711d63525921b8125df224ecfb1518cdb1ee309
Getaneh48 <getaneh11@gmail.com>
2022-09-19 13:35:29 -0700

    Task 6 bug fixed

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,34 +10,18 @@
 
 void puts2(char *str)
 {
-
-	while (*str)
-	{
-		_putchar(*str);
-		str += 2;
-	}
-
-	_putchar('\n');
-}
-
-/**
-* _strlen -  returns the length of a string.
-*
-* @s: character pointer
-*
-* Return: integer length of a string.
-*
-*/
-int _strlen(char *s)
-{
 	int i;
 
 	i = 0;
-	while (*s)
+	while (*str != '\0')
 	{
-		i++;
-		s += 1;
+		if (i % 2 == 0)
+		{
+			_putchar(*str);
+		}
+		str += 1;
+		i += 1;
 	}
 
-	return (i);
+	_putchar('\n');
 }
