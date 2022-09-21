9241eb5b0eff8328c568052fcafcd622e2125bba
Getaneh48 <getaneh11@gmail.com>
2022-09-21 15:24:08 +0300

    Project 0x06

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,76 @@
+#include "main.h"
+
+/**
+* cap_string -  capitalizes all words of a string.
+*
+* @s: string to be capitalize
+*
+* Return: char pointer of the capitalized string.
+*
+*/
+
+char *cap_string(char *s)
+{
+	int c, i;
+	int separators[] = {9, 10, 32, 44, 59, 46, 33, 63, 34, 40, 41, 123, 125};
+	const int SIZE = 13;
+
+	c = i = 0;
+
+	while (*s != '\0')
+	{
+		c = (int) *s;
+		if (c >= 97 && c <= 122)
+		{
+			if (i > 0)
+			{
+				if (_inarray(separators, SIZE, (int) *(s - 1)))
+				{
+					c = c - 32;
+					*s = (char) c;
+				}
+			}
+			else
+			{
+				if (_inarray(separators, SIZE, (int) *s))
+				{
+					c = c - 32;
+					*s = (char) c;
+				}
+			}
+		}
+		s += 1;
+		i++;
+	}
+
+	return (s);
+}
+
+/**
+* _inarray - check if a number is in the array
+*
+* @arr: array to be searched in
+* @len: length of the array.
+* @n: the number to be searched in the array
+*
+* Return: 1 if found else 0.
+*
+*/
+
+int _inarray(int *arr, int len, int n)
+{
+	int i, found;
+
+	i = found = 0;
+
+	for (; i < len; i++)
+	{
+		if (arr[i] == n)
+		{
+			found = 1;
+			break;
+		}
+	}
+
+	return (found);
+}
---
+++ 0x06-pointers_arrays_strings/main.h
@@ -0,0 +1,4 @@
+#include <stdio.h>
+int _putchar(char c);
+char *cap_string(char *);
+int _inarray(int *arr, int len, int n);
