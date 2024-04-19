65a3faa5fbf04a1d4be5546d2838b7a9609a8aad
Getaneh48 <getaneh11@gmail.com>
2024-04-19 19:38:09 +0000

    a puppet manifest file to create a file in temp folder

---
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -0,0 +1,8 @@
+# a puppet manifest file that create a file in /tmp folder and fullfils the requirement
+file { '/tmp/school':
+  ensure  => present,
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744',
+  content => 'I love Puppet',
+}
