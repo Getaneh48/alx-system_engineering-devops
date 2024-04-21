bbd793ba2d25a6577069187659a5848ce6d1de5e
Getaneh48 <getaneh11@gmail.com>
2024-04-21 17:31:52 +0000

    modified

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,16 +1,14 @@
 # setup ssh client config - diable password authentication and use an identity file given
 include stdlib
 
-file_line {'Turn off passwd auth':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no'
-  replace => true,
-}
-
-file_line {'Declare identity file':
-  ensure  => present,
-  path    => '/etc/ssh/ssh_config',
-  line    => '     IdentityFile ~/.ssh/school',
-  replace => true,
+file_line {
+  'Turn off passwd auth':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '     PasswordAuthentication no'
+  ;
+  'Declare identity file':
+    ensure => present,
+    path   => '/etc/ssh/ssh_config',
+    line   => '     IdentityFile ~/.ssh/school',
 }
