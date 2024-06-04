58b6a32ebb3698ab802d9ce8a14b67791739a38a
Getaneh48 <getaneh11@gmail.com>
2024-06-04 14:21:18 +0000

    fixed error

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -3,25 +3,29 @@
 a function that queries the Reddit API and prints the titles of the first
 10 hot posts listed for a given subreddit.
 """
+
 import requests
 
 
 def top_ten(subreddit):
+    """Request top ten hot posts of subreddit
+    from Reddit API
     """
-    prints the titles of the first 10 hot posts listed for a given subreddit.
-    """
+    # set custom user-agent
+    user_agent = '0x16-api_advanced-jmajetich'
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
 
-    url = "https://www.reddit.com/r/{}/hot/.json".format(subreddit)
-    headers = {
-        "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/bdov_)"
-    }
-    params = {
-        "limit": 10
-    }
-    response = requests.get(url, headers=headers, params=params,
-                            allow_redirects=False)
-    if response.status_code == 404:
-        print("None")
-        return
-    results = response.json().get("data")
-    [print(cnt.get("data").get("title")) for cnt in results.get("children")]
+    # custom user-agent avoids request limit
+    headers = {'User-Agent': user_agent}
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
+    if r.status_code != 200:
+        print('None')
+    else:
+        # load response unit from json
+        data = r.json()['data']
+        # extract list of pages
+        posts = data['children']
+        for post in posts:
+            print(post['data']['title'])
